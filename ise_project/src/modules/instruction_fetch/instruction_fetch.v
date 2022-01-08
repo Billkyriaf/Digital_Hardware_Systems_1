66ed7878a5839c77f541c2d897d4008c1b504854
ErikaKoro <92887290+ErikaKoro@users.noreply.github.com>
2022-01-08 22:15:01 +0200

    Update instruction_fetch.v

--- ise_project/src/modules/instruction_fetch/instruction_fetch.v
+++ ise_project/src/modules/instruction_fetch/instruction_fetch.v
@@ -19,12 +19,43 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module instruction_fetch(
-    input RF_B_sel,
-    input Clk,
-    output reg [31:0] Immed,
-    output reg [31:0] RF_A,
-    output reg [31:0] RF_B
+    input [31:0] PC_Immed;
+    input PC_sel;
+    input PC_LdEn;
+    input Reset;
+    input Clk;
+    output reg[31:0] Instr;
     );
 
+    reg [31:0] PC;
+    wire [2*32 - 1:0] mux_input;
 
+    IMEM imem(
+        .clk(Clk),
+        .addr(PC[11:2]),
+        .dout(Instr)
+    );
+
+    mux_32_1 #(.BUS_WIDTH(32), .SEL(1)) mux(
+        .Din(mux_input),
+        .Sel(PC_sel),
+        .Dout(PC)
+    );
+
+    always @(posedge Clk) begin
+        if(Reset == 1) begin
+            PC = 0;
+        end else if (PC_LdEn == 1) begin
+            assign mux_input[31:0] = PC + 4;
+            assign mux_input[63:32] = PC + 4 + PC_Immed;            
+        end else begin
+            PC = PC;
+        end
+
+        
+    end
+
+  
 endmodule
+
+
